1a64ba7e7ac09698a35c31a6bc3cd32010f6bc4a
puneeths1607 <puneeths1607@gmail.com>
2026-04-20 11:28:13 +0530

    hii

---
+++ script.js
@@ -0,0 +1,132 @@
+// var name = "puneeths" ;
+// console.log(name);
+
+// let Name ;
+// Name = "puneeths" ;
+// console.log(Name);
+
+// what is array ?
+// array is a collection of values
+// array is a list of values
+//              0        1        2       3
+// var Names =  ["sheela","leela","geetha","mala","kamla"] ;
+
+// console.log("sheela","leela","geetha","mala");
+// console.log(Names[0]);
+// console.log(Names[1]);
+// console.log(Names[2]);
+// console.log(Names[3]);
+// console.log(Names[4])
+
+// operator
+// arthmetic operator  =>  + - * / %
+// assignment operator =>  = += -= *= /= %=
+// comparison operator => = == != > < >= <= ===
+// logical operator    =>  && || !
+// bitwise operator    =>  & | ~ ^ << >> >>>
+
+// types of statements
+// conditional statements
+
+// if
+// else
+// else if
+
+// loop statements
+
+// for
+// while
+// do while
+
+// = is assignment operator  used to assign the values
+// == is comparison operator used to compare the values
+//  only the values are compared not the data types
+
+// === it is used to compare the values and data types
+
+// let num1 = "10";
+// let num2 = 10;
+// if (num1 === num2) {
+//   console.log("hii this is true 1");
+// } else if (num1 == num2) {
+//   console.log("hii this is true 2");
+// } else {
+//   console.log("hii this is false");
+// }
+
+
+
+
+// loops
+
+// let array = [1,2,3,4,5,6,7,8,9,10];
+
+//  methods are  nothing but predefined functions which are used to perform the tasks
+
+
+
+
+//  index condition increment
+// let i = 2;
+// for(  i ; i <= 4; i++) {
+// console.log(array[i])
+// }
+
+
+// array methods
+// let array = [1,5,7,8,9];
+
+// length
+// console.log("length of the array",array.length);
+// pop = is used to remove the last element
+// console.log(array.pop())
+// console.log(array);
+// // remove the first element
+// console.log(array.shift())
+// console.log(array);
+
+
+// // push = is used to add the element at the last
+// console.log(array.push(11))
+// console.log(array);
+// //  unshift = is used to add the element at the first
+// console.log(array.unshift(0))
+// console.log(array);
+
+// // reverse = is used to reverse the array
+// console.log(array.reverse())
+// console.log(array);
+
+// sort = is used to sort the array
+// console.log(array.sort())
+// console.log(array);
+
+
+// slice = is used to slice the array
+// console.log(array.slice(0,3))
+// console.log(array);
+
+
+// splice = is used to remove the element from the array
+// console.log(array.splice(0,3))
+// console.log(array);
+
+// indexOf = is used to find the index of the element
+// console.log(array.indexOf(5))
+
+
+console.log("puneeths")
+
+// print on brouser
+
+
+
+// Dom = document object model
+
+// we have 5 different types of dom elements
+
+// 1. document.getElementById
+// 2. document.getElementsByClassName
+// 3. document.getElementsByTagName
+// 4. document.querySelector
+// 5. document.querySelectorAll
